592da63fe2c3a912c6b25fde1d883479dc5b9423
adeel-25 <adeelashraf.9952@gmail.com>
2026-03-09 19:54:13 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -15,37 +15,26 @@ ctx.beginPath();
 ctx.arc(700, 80, 40, 0, Math.PI * 2);
 ctx.fillStyle = "yellow";
 ctx.fill();
+ctx.stroke();
 
 // Caption text
 ctx.fillStyle = "black";
 ctx.font = "20px Arial";
 ctx.fillText("My Robot Cartoon", 320, 40);
 
-// Robot body
-ctx.fillStyle = "gray";
-ctx.fillRect(350, 200, 100, 120);
-
 // Robot head
 ctx.fillStyle = "silver";
 ctx.fillRect(360, 150, 80, 50);
+ctx.strokeRect(360, 150, 80, 50);
 
-// Robot eyes
+// Eyes
 ctx.fillStyle = "blue";
 ctx.fillRect(375, 165, 15, 15);
 ctx.fillRect(410, 165, 15, 15);
 
-// Robot mouth
+// Mouth
 ctx.fillStyle = "black";
-ctx.fillRect(385, 190, 40, 5);
-
-// Robot arms
-ctx.fillStyle = "gray";
-ctx.fillRect(320, 210, 30, 10);
-ctx.fillRect(450, 210, 30, 10);
-
-// Robot legs
-ctx.fillRect(370, 320, 15, 40);
-ctx.fillRect(415, 320, 15, 40);
+ctx.fillRect(385, 190, 30, 5);
 
 // Antenna
 ctx.beginPath();
@@ -57,13 +46,35 @@ ctx.beginPath();
 ctx.arc(400, 125, 5, 0, Math.PI * 2);
 ctx.fillStyle = "red";
 ctx.fill();
+ctx.stroke();
 
+// Robot body
+ctx.fillStyle = "gray";
+ctx.fillRect(350, 200, 100, 120);
+ctx.strokeRect(350, 200, 100, 120);
 
-// Fence using for loop + translate
+// Left arm
+ctx.fillRect(320, 220, 30, 10);
+ctx.strokeRect(320, 220, 30, 10);
+
+// Right arm
+ctx.fillRect(450, 220, 30, 10);
+ctx.strokeRect(450, 220, 30, 10);
+
+// Left leg
+ctx.fillRect(370, 320, 15, 40);
+ctx.strokeRect(370, 320, 15, 40);
+
+// Right leg
+ctx.fillRect(415, 320, 15, 40);
+ctx.strokeRect(415, 320, 15, 40);
+
+// Fence using for loop with translate
 for (let i = 0; i < 8; i++) {
     ctx.save();
     ctx.translate(i * 60, 0);
     ctx.fillStyle = "brown";
     ctx.fillRect(50, 320, 10, 30);
+    ctx.strokeRect(50, 320, 10, 30);
     ctx.restore();
 }
