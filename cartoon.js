fa994e1d784c69f20a327e6bd1a2dfe8c82e0964
adeel-25 <adeelashraf.9952@gmail.com>
2026-03-09 19:49:06 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -1,59 +1,69 @@
-// get canvas
+// Get canvas and context
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-// sky background
-ctx.fillStyle = "skyblue";
-ctx.fillRect(0,0,600,400);
+// Sky background
+ctx.fillStyle = "lightblue";
+ctx.fillRect(0, 0, 800, 400);
 
-// ground
+// Ground
 ctx.fillStyle = "green";
-ctx.fillRect(0,300,600,100);
+ctx.fillRect(0, 350, 800, 50);
 
-// sun
+// Sun
 ctx.beginPath();
-ctx.arc(500,80,40,0,Math.PI*2);
+ctx.arc(700, 80, 40, 0, Math.PI * 2);
 ctx.fillStyle = "yellow";
 ctx.fill();
+
+// Caption text
+ctx.fillStyle = "black";
+ctx.font = "20px Arial";
+ctx.fillText("My Robot Cartoon", 320, 40);
+
+// Robot body
+ctx.fillStyle = "gray";
+ctx.fillRect(350, 200, 100, 120);
+
+// Robot head
+ctx.fillStyle = "silver";
+ctx.fillRect(360, 150, 80, 50);
+
+// Robot eyes
+ctx.fillStyle = "blue";
+ctx.fillRect(375, 165, 15, 15);
+ctx.fillRect(410, 165, 15, 15);
+
+// Robot mouth
+ctx.fillStyle = "black";
+ctx.fillRect(385, 190, 40, 5);
+
+// Robot arms
+ctx.fillStyle = "gray";
+ctx.fillRect(320, 210, 30, 10);
+ctx.fillRect(450, 210, 30, 10);
+
+// Robot legs
+ctx.fillRect(370, 320, 15, 40);
+ctx.fillRect(415, 320, 15, 40);
+
+// Antenna
+ctx.beginPath();
+ctx.moveTo(400, 150);
+ctx.lineTo(400, 130);
 ctx.stroke();
 
-// house
-ctx.fillStyle = "brown";
-ctx.fillRect(200,200,200,100);
-
-// roof
 ctx.beginPath();
-ctx.moveTo(200,200);
-ctx.lineTo(300,130);
-ctx.lineTo(400,200);
-ctx.closePath();
-ctx.fillStyle="red";
+ctx.arc(400, 125, 5, 0, Math.PI * 2);
+ctx.fillStyle = "red";
 ctx.fill();
 
-// door
-ctx.fillStyle="black";
-ctx.fillRect(285,240,30,60);
 
-// windows
-ctx.fillStyle="lightblue";
-ctx.fillRect(220,220,40,30);
-ctx.fillRect(340,220,40,30);
-
-// caption text
-ctx.fillStyle="black";
-ctx.font="20px Arial";
-ctx.fillText("My Cartoon House",220,30);
-
-// grass using for loop + translate
-for(let i=0;i<20;i++){
+// Fence using for loop + translate
+for (let i = 0; i < 8; i++) {
     ctx.save();
-    ctx.translate(i*30,0);
-
-    ctx.beginPath();
-    ctx.moveTo(10,300);
-    ctx.lineTo(15,285);
-    ctx.lineTo(20,300);
-    ctx.stroke();
-
+    ctx.translate(i * 60, 0);
+    ctx.fillStyle = "brown";
+    ctx.fillRect(50, 320, 10, 30);
     ctx.restore();
 }
